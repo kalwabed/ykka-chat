1c62e2882f4ddfd7f72c7e76c2c084a2305f048f
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-09-07 23:55:07 +0700

    feat: mobile alert

---
+++ src/components/mobile-alert.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hfull flex items-center wrapper">
+    <div
+      class="flex p4 mx4 wfull flex-col bg-white rd-lg b-gray1 shadow backdrop-filter backdrop-blur-4 bg-opacity-2 justify-center text-center items-center"
+    >
+      <div class="inline-flex gap2 mb4">
+        <i class="i-tabler:mood-sad-squint w12 h12"></i>
+        <i class="i-tabler:skateboarding w12 h12"></i>
+        <i class="i-tabler:aperture w12 h12"></i>
+        <i class="i-tabler:anchor w12 h12"></i>
+        <i class="i-tabler:abacus w12 h12"></i>
+        <i class="i-tabler:backpack w12 h12"></i>
+      </div>
+      <small>YKKA Chat adalah aplikasi chat iseng-iseng.</small>
+      <small>Saat ini belum mendukung perangkat mobile. Hehe.</small>
+
+      <b class="mt4">Terima kasih</b>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background-image: url(grain.svg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
 import LoginForm from '@/components/login-form.vue'
 import ChatCore from '@/components/chats/chat-core.vue'
+import MobileAlert from '@/components/mobile-alert.vue'
+
+const isMobile = ref(false)
 
 const userStore = useUserStore()
+const breakpoints = useBreakpoints({
+  tablet: 640,
+  laptop: 1024,
+  desktop: 1280
+})
+
+onBeforeMount(() => {
+  if (breakpoints.isSmaller('tablet')) {
+    isMobile.value = true
+  }
+})
 </script>
 
 <template>
-  <ChatCore v-if="userStore.currentUser?.id" />
-  <LoginForm v-else />
+  <MobileAlert v-if="isMobile" />
+  <template v-else>
+    <ChatCore v-if="userStore.currentUser?.id" />
+    <LoginForm v-else />
+  </template>
 </template>
