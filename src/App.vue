3448506106911c583e0dc3e6fac3c74727e662d4
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-09-10 15:31:49 +0700

    feat: new scrollbar for chrome

--- src/App.vue
+++ src/App.vue
@@ -21,4 +21,26 @@ body {
   background-size: cover;
   background-color: black;
 }
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: rgba(17, 24, 39, 1);
+}
+
+::-webkit-scrollbar {
+  width: 6px;
+  background-color: rgba(17, 24, 39, 0);
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: rgba(156, 163, 175, 0.3);
+}
+
+::-webkit-scrollbar-thumb:focus {
+  background-color: rgba(156, 163, 175, 0.3);
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: rgba(156, 163, 175, 0.2);
+}
 </style>
